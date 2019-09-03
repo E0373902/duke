54acbc2269fac3dfc0b72d3a126f50051adec9d4
ShriyaSaxena <e0373902@u.nus.edu>
2019-09-04 00:10:37 +0800

    Level 3:Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,43 @@
+import main.java.Task;
+
 import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Duke\n" + "What can I do for you?");
-        List<String>list = new ArrayList<String>();
+        ArrayList<Task> myArray = new ArrayList();
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
         Scanner input = new Scanner(System.in);
         while (true) {
             String s = input.nextLine();
             if (s.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you soon again.");
                 break;
-            }
-            if (s.equals("list")) {
-                for(int j = 0; j < list.size(); j = j + 1){
-                    System.out.println((j + 1) + "." + list.get(j));
-                }
             } else {
-                list.add(s);
-                System.out.println("added:" + s);
+                if (s.equals("list")) {
+                    Task r = new Task("");
+                    for (int i = 0; i < myArray.size(); i = i + 1) {
+                        r = myArray.get(i);
+                        System.out.println((i + 1) + ".[" + r.getStatusIcon() + "]" + r.description);
+                    }
+                } else {
+                    Task t = new Task(s);
+                    myArray.add(t);
+                    Task f = new Task("");
+                    for (int i = 0; i < myArray.size(); i = i + 1) {
+                        if (s.equals("Done " + (i + 1))) {
+                            myArray.remove(myArray.size() - 1);
+                            f = myArray.get(i);
+                            f.markAsDone();
+                            System.out.println("Nice! I've marked this task as done: ");
+                            System.out.println((i + 1) + ".[" + f.getStatusIcon() + "]" + f.description);
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+
+
+
